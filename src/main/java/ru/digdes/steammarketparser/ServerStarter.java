13795602496d48099f118a34bfcc1a95aaf8ea30
Airat Khazipov <qwadr2014@yandex.ru>
2017-04-04 21:14:49 +0300

    initial commit

---
+++ src/main/java/ru/digdes/steammarketparser/ServerStarter.java
@@ -0,0 +1,17 @@
+package ru.digdes.steammarketparser;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class ServerStarter {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println("Когда-нибудь здесь стартанёт сервак.");
+            System.out.println("Надеемся, это будет скоро.");
+        }
+    }
+}
